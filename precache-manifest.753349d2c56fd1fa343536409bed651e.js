1349c896caa672eb38336cfb76f7888f9f58ba0a
MuskanBansal3 <muskanbansal369@gmail.com>
2020-05-30 23:14:43 +0530

    Updates

---
+++ precache-manifest.753349d2c56fd1fa343536409bed651e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "359d89f6611965b6c2c7c4942160fb1d",
+    "url": "/evaluation-react-project1/index.html"
+  },
+  {
+    "revision": "960e8fdd2dad4a9a803d",
+    "url": "/evaluation-react-project1/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ee05ceca86b43f2c4ccb",
+    "url": "/evaluation-react-project1/static/js/2.33c53203.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/evaluation-react-project1/static/js/2.33c53203.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "960e8fdd2dad4a9a803d",
+    "url": "/evaluation-react-project1/static/js/main.186397e2.chunk.js"
+  },
+  {
+    "revision": "7d00d9fca898dffb339a",
+    "url": "/evaluation-react-project1/static/js/runtime-main.833e6052.js"
+  }
+]);
